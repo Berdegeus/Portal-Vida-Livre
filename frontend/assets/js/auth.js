b314610bb5e8ff6fb147b42bbe97a72a32c9e862
Alana Vasconcelos <alanak.av@gmail.com>
2026-04-09 18:31:40 -0300

    ajustando pagina de sec pra deixar mais facil p usuario entender

--- frontend/assets/js/auth.js
+++ frontend/assets/js/auth.js
@@ -268,6 +268,7 @@ const isValidTotpCode = (code) => /^\d{6}$/.test(code);
     showMessage,
     clearMessage,
     clearFieldErrors,
+    setFieldError,
     applyErrors,
     isValidEmail,
     isValidName,
